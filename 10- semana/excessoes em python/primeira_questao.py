0b34ecbb7d414a6a077ba359f90ba19a7f3c3173
Vagner Alves Ferreira da Silva <vafs@palmares.ifpe.edu.br>
2024-09-04 15:58:29 -0300

    excessões em python

--- 10- semana/excessoes em python/primeira_questao.py
+++ 10- semana/excessoes em python/primeira_questao.py
@@ -1,4 +1,4 @@
-class listagem_numerica:
+class Listagem_numerica:
 
     def __init__(self, lista):
         self.lista = lista
@@ -6,7 +6,7 @@ class listagem_numerica:
     def obter_numero_pelo_indice(self, indice):
         
         try:
-            self.lista[indice]
+            return self.lista[indice]
 
         except IndexError:
             print("Indice Inválido")
@@ -18,3 +18,7 @@ class listagem_numerica:
 
 
 numeros = [1,2,3,4,5,6,7,8,9,10]
+
+listagem_numerica = Listagem_numerica(numeros)
+
+resultado_busca = listagem_numerica.obter_numero_pelo_indice(2)
